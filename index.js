f5ea0f8bb04fc610fd7944443fd9cf1f7cde23d9
Harsha Anuga <harshaanuga358@gmail.com>
2025-02-10 19:07:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const zod = require("zod");
+const app = express();
+
+const schema = zod.array(zod.number());
+app.use(express.json());
+
+app.post("/", function (req, res) {
+  const kidneys = req.body.kidneys;
+  const response = schema.safeparse(kidneys);
+  response.send({
+    response,
+  });
+}),
+  app.listen(3000);
